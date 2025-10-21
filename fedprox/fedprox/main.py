c8e1b1f0e1801bac8e962f583b4dac9a7c2320c0
sany13-94 <sanaalkrn@gmail.com>
2025-10-21 20:45:59 +0000

    uxfsbvp

--- fedprox/fedprox/main.py
+++ fedprox/fedprox/main.py
@@ -290,7 +290,7 @@ def get_server_fn(mlflow=None):
       )
 
     # Configure the server for 5 rounds of training
-    config = ServerConfig(num_rounds=30)
+    config = ServerConfig(num_rounds=100)
     return ServerAppComponents(strategy=strategyi, config=config)
  return server_fn
 
