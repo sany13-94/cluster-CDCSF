d796ab7f6d8bc9aea9d2d0466df09d09b83ba971
sany13-94 <sanaalkrn@gmail.com>
2025-11-22 19:17:58 +0000

    add fairness per cluster

--- fedprox/fedprox/main.py
+++ fedprox/fedprox/main.py
@@ -342,10 +342,10 @@ def get_server_fn(mlflow=None):
     ckpt = load_latest_checkpoint(prev_ckpt_dir)
 
     initial_parameters = None
-    base_round = 0  # default: brand new training
+    base_round = 4  # default: brand new training
 
     # Total global rounds across ALL runs
-    global_total_rounds = 4  #8 or cfg.num_rounds
+    global_total_rounds = 8  #8 or cfg.num_rounds
 
     if ckpt is not None:
             initial_parameters = ckpt["parameters"]
