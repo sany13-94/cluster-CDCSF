9f41c75009770a7384c5cdbecb4c4e369a9f1b55
sany13-94 <sanaalkrn@gmail.com>
2025-12-05 10:57:45 +0000

    add heatmap visualization

--- fedprox/fedprox/main.py
+++ fedprox/fedprox/main.py
@@ -345,7 +345,7 @@ def get_server_fn(mlflow=None):
     base_round = 0  # default: brand new training
 
     # Total global rounds across ALL runs
-    global_total_rounds = 200  #8 or cfg.num_rounds
+    global_total_rounds = 130  #8 or cfg.num_rounds
 
     if ckpt is not None:
             initial_parameters = ckpt["parameters"]
